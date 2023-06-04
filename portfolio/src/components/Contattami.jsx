b80bb0394459123b59b356758500319a53318bee
DavideCruciani <cruciani240203@gmail.com>
2023-06-05 00:53:51 +0200

    minor style changes
    minor style changes

--- portfolio/src/components/Contattami.jsx
+++ portfolio/src/components/Contattami.jsx
@@ -9,11 +9,11 @@ const Contattami = () => {
       <div ref={myRef} className={myElementIsVisible ? 'container h-full flex items-center mx-auto px-4 opacity-100 blur-0 duration-[1s] delay-[250ms]' : 'container h-full flex items-center mx-auto px-4 opacity-0 blur-[5px] bg-transparent'}>
           <form className='flex flex-col gap-3 mx-auto max-w-[600px] w-full' action="https://getform.io/f/8de1651c-4bc5-4435-9f7b-cc69853be0f6" method='POST'>
             <h1 className='text-white text-5xl sm:text-6xl md:text-7xl text-center font-Marker'>Contattami</h1>
-            <p className='text-white text-lg mb-8'>// Compila questo modulo o contattami a questa email - <span className='font-bold italic'>cruciani240203@gmail.com</span></p>
+            <p className='text-white text-lg mb-8'>// Compila questo modulo o contattami a questa email - <span className='font-bold italic text-[#3B82F6]'>cruciani240203@gmail.com</span></p>
             <input className='p-2 rounded-md' type="text" name="name" placeholder='Nome' required />
             <input className='p-2 rounded-md' type="email" name="email" placeholder='Email' required />
             <textarea className='p-2 rounded-md resize-none' name="message" cols="30" rows="10" placeholder='Messaggio' required></textarea>
-            <button className='bg-[#1C1C1C] text-white py-2 font-Marker rounded-md active:bg-[#5e5e5e]' type="submit">INVIA</button>
+            <button className='bg-[#3B82F6] text-white py-2 font-Marker rounded-md active:bg-[#5e5e5e]' type="submit">INVIA</button>
           </form>
         
       </div>
--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -16,35 +16,35 @@ const Skills = ({illustrazione}) => {
           <h1 ref={myRef} className={myElementIsVisible ? 'text-white text-5xl sm:text-6xl md:text-7xl text-center font-Marker py-12 opacity-100 blur-0 duration-[2s]' : 'text-white text-5xl sm:text-6xl md:text-7xl text-center font-Marker py-12 opacity-0 blur-[5px] bg-transparent'}>LE MIE SKILLS</h1>
           <div className='w-full grid gap-3 md:gap-6 grid-rows-4 grid-cols-2 md:grid-rows-3 md:grid-cols-3 lg:grid-rows-2 lg:grid-cols-4 font-Marker'>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[150ms]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-html5-plain colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-html5-plain colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>HTML5</p>
             </div>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[.3s]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-css3-plain colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-css3-plain colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>CSS3</p>
             </div>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[0.45s]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-javascript-plain colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-javascript-plain colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>JAVASCRIPT</p>
             </div>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[.6s]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-react-original colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-react-original colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>REACT</p>
             </div>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[.75s]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-typescript-plain colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-typescript-plain colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>TYPESCRIPT</p>
             </div>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[.9s]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-github-original colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-github-original colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>GITHUB</p>
             </div>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[1.05s]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-bootstrap-plain colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-bootstrap-plain colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>BOOTSTRAP</p>
             </div>
             <div ref={myRef} className={myElementIsVisible ? 'text-center opacity-100 blur-0 duration-[1s] delay-[1.2s]' : 'text-center opacity-0 blur-[5px] duration-[1s] bg-transparent'}>
-              <i className="devicon-tailwindcss-plain colored text-7xl sm:text-8xl md:text-9xl"></i>
+              <i className="devicon-tailwindcss-plain colored text-7xl sm:text-8xl md:text-9xl hover:animate-pulse cursor-grab"></i>
               <p className='text-white md:text-lg font-bold mt-3'>TAILWIND</p>
             </div>
           </div>
