1de8a9160373cc5259b3a97df7e5e8a13c23115f
DavideCruciani <cruciani240203@gmail.com>
2023-07-14 19:00:34 +0200

    changed foodiebox project
    changed foodiebox project with hoobank project

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer"
 import logo from "./assets/logo.png"
 import illustrazione from './assets/Studying.svg'
 import screenBMW from './assets/projects/Homepage-BMW.jpg'
-import screenFoodiebox from './assets/projects/Foodiebox.png'
+import screenHooBank from './assets/projects/HooBank.png'
 import screenSpaceTourism from './assets/projects/Space-tourism.jpg'
 import screenTracker from './assets/projects/geolocator.png'
 import arrow from './assets/up-arrow.svg'
@@ -21,7 +21,7 @@ function App() {
     <Navbar logo={logo}/>
     <Hero />
     <Skills illustrazione={illustrazione}/>
-    <Progetti project1={screenBMW} project2={screenFoodiebox} project3={screenSpaceTourism} project4={screenTracker}/>
+    <Progetti project1={screenBMW} project2={screenHooBank} project3={screenSpaceTourism} project4={screenTracker}/>
     <Contattami />
     <Footer arrow={arrow}/>
     </>
--- portfolio/src/components/Progetti.jsx
+++ portfolio/src/components/Progetti.jsx
@@ -38,23 +38,23 @@ const Progetti = ({project1, project2, project3, project4}) => {
               <div className='absolute w-full h-full bg-[hsl(196,38%,17%)]/[.75] group-hover:opacity-100 opacity-0'>
                 <div className='flex flex-col justify-between h-full w-full p-5'>
                   <div>
-                    <h2 className='text-white text-2xl md:text-4xl lg:text-5xl font-Anton'>FOODIEBOX</h2>
+                    <h2 className='text-white text-2xl md:text-4xl lg:text-5xl font-Anton'>HOOBANK</h2>
                     <div className='flex gap-4 py-3'>
                       <i className="devicon-react-original colored text-3xl"></i>
-                      <i className="devicon-bootstrap-plain colored text-3xl"></i>
+                      <i className="devicon-tailwindcss-plain colored text-3xl"></i>
                     </div>
                   </div>
                   <div className='flex'>
-                    <a href='https://foodiebox.netlify.app/' target='_blank'>
+                    <a href='https://hoobankdc.netlify.app/' target='_blank'>
                       <button className='bg-white text-blue-500 hover:bg-gray-300 py-2 px-3 rounded-md mr-4'>LIVE DEMO</button>
                     </a>
-                    <a href='https://github.com/DavideCruciani/Foodiebox' target='_blank'>
+                    <a href='https://github.com/DavideCruciani/bank_modern_app' target='_blank'>
                       <button className='bg-blue-500 text-white hover:bg-blue-600 py-2 px-3 rounded-md'>CODE</button>
                     </a>
                   </div>
                 </div>
               </div>
-              <img src={project2} alt="Foodiebox website" />
+              <img src={project2} alt="HooBank website" />
             </div>
             {/* THIRD PROJECT */}
             <div className='max-w-[550px] mx-auto relative group'>
